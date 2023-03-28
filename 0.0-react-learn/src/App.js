30d1875e7b4e1ec5ac989d3af1fc2e7c8894408a
niki <nikiwone@gmail.com>
2023-03-28 03:25:21 +0300

    🚀 added 0.0-react-learn, i did Creating and nesting components, Writing markup with JSX, Adding styles, Displaying data

---
+++ 0.0-react-learn/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+/* 
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+      <AboutPage />
+    </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there. <br />How do you do</p>
+    </>
+  );
+}
+*/
