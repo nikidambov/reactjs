08ab5780a6acc15649ba2099a8d340366d555765
niki <nikiwone@gmail.com>
2023-11-06 12:42:21 +0200

    added 03.Components-Deep-Dive components-deep-dive

---
+++ ReactJS-October-2023/03.Components-Deep-Dive/components-deep-dive/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function App() {
+    const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+
+
+    const onClick = () => {
+        setNumbers(oldState => oldState.slice(1));
+    }
+    return (
+        <div>
+            <ul>
+                {numbers.map((number, index) => <li data-key={index} key={index}>{number * 2}</li>)}
+            </ul>
+            <button onClick={onClick}>Remove</button>
+        </div>
+    );
+}
+
+export default App
