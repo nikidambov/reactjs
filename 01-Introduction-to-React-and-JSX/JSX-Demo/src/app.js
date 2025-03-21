4c5bc7b4c8af259f5eb0af8e0b9742dbe5b860b1
niki <nikiwone@gmail.com>
2025-03-21 19:45:44 +0200

    Intro to JSX

--- 01-Introduction-to-React-and-JSX/JSX-Demo/src/app.js
+++ 01-Introduction-to-React-and-JSX/JSX-Demo/src/app.js
@@ -4,13 +4,22 @@ const rootDomElement = document.getElementById("root");
 // Create Root
 const reactRootElement = ReactDOM.createRoot(rootDomElement);
 
-// Create React Element
+// Create React Element without JSX
 const headingReactElement = React.createElement("h1", {}, "Hello from React!");
 const subHeadingReactElement = React.createElement("h2", {id: "sub-header"}, "The Best Framework!");
 const headerSectionReactElement = React.createElement("header", {}, headingReactElement, subHeadingReactElement);
 
+// Create React Element with JSX
+const headerSectionReactJSXElement = (
+    <header>
+        <h1 className="main-header">Hello from JSX!</h1>
+        <h2 id="sub-header">The best Superset Language!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, fuga?</p>
+    </header>
+);
+
 // Render Element To UI
-reactRootElement.render(headerSectionReactElement);
+reactRootElement.render(headerSectionReactJSXElement);
 
 // Compare React Element VS DOM Element
 setTimeout(() => {
